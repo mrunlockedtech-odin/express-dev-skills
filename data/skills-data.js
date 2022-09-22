fc99c022dee79bf789d4338a4145e3604225f787
mrunlockedtech <jrbhrn@gmail.com>
2022-09-21 22:19:33 -0500

    Add inital array data and stub up skills/index.ejs

---
+++ data/skills-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {name: 'HTML', learned: true, unit:1}
+  {name: 'CSS', learned: true, unit:1}
+  {name: 'Javascript', learned: true, unit:1}
+  {name: 'Node', learned: false, unit:1}
+  {name: 'Express', learned: false, unit:1}
+  {name: 'MongoDB', learned: false, unit:1}
+  {name: 'Mongoose', learned: false, unit:1}
+]
