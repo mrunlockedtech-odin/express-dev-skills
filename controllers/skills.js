22231b097ab1e6ec341444002b99e9cf1213b67a
mrunlockedtech <jrbhrn@gmail.com>
2022-09-22 21:45:47 -0500

    Add create functionality and fix variable in index.ejs

--- controllers/skills.js
+++ controllers/skills.js
@@ -16,7 +16,20 @@ function newSkill(req,res){
   res.render('skills/new')
 }
 
+function create(req,res){
+console.log(req.body)
+Skill.create(req.body)
+.then(skill => {
+  res.redirect('/skills')
+})
+.catch(error => {
+  console.log(error)
+  res.redirect('/')
+})
+}
+
 export{
   index,
   newSkill as new,
+  create,
 }
--- models/skill.js
+++ models/skill.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const skillSchema = new Schema({
-  name:String,
+  text:String,
   learned:Boolean,
   unit:Number,
 })
